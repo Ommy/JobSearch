610f54b7c32baed371928491bb66625feaf8056e
Fasih Awan <fasihawan@gmail.com>
2012-08-17 17:02:20 -0400

    Added internet checks

--- src/com/fasih/jobsearch/Base.java
+++ src/com/fasih/jobsearch/Base.java
@@ -1,9 +1,13 @@
 package com.fasih.jobsearch;
 
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.app.Activity;
+import android.graphics.Color;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 import android.support.v4.app.NavUtils;
 
 public class Base extends Activity {
@@ -12,6 +16,20 @@ public class Base extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_base);
+        TextView tv = (TextView)findViewById(R.id.tV);
+        ConnectivityManager cMan = (ConnectivityManager)getSystemService(CONNECTIVITY_SERVICE);
+        NetworkInfo nInfo = cMan.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+        if (nInfo.isConnected())
+        {
+        	tv.setTextColor(Color.GREEN);
+        	tv.setText("Connected");
+        }
+        else
+        {
+        	tv.setTextColor(Color.RED);
+        	tv.setText("Not Connected");
+        }
+        	
     }
 
     @Override
