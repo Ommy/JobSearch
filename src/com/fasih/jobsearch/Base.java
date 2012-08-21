8557e5f038b9a4a37615bf0feb6c9fcd5d6744c4
Fasih Awan <fasihawan@gmail.com>
2012-08-21 10:31:42 -0400

    Cleaned up code, made a getter

--- src/com/fasih/jobsearch/Base.java
+++ src/com/fasih/jobsearch/Base.java
@@ -23,58 +23,84 @@ import android.support.v4.app.NavUtils;
 
 public class Base extends Activity {
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_base);
-        TextView tv = (TextView)findViewById(R.id.tV);
-        ConnectivityManager cMan = (ConnectivityManager)getSystemService(CONNECTIVITY_SERVICE);
-        NetworkInfo nInfo = cMan.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
-        if (nInfo.isConnected())
-          	tv.setText("Connected"); 
-        else
-        	tv.setText("Not Connected");
-        LocationManager lM = (LocationManager)this.getSystemService(Context.LOCATION_SERVICE);
-        Location userLocation = lM.getLastKnownLocation(lM.NETWORK_PROVIDER);
-        Location planTwo = lM.getLastKnownLocation(lM.GPS_PROVIDER);
-        Geocoder gC = new Geocoder(getBaseContext(),Locale.getDefault());
-        try {
-			Log.d("LOC_DATA", (gC.getFromLocation(planTwo.getLatitude(),planTwo.getLongitude(),1).get(0).toString()));
-		} catch (IOException e1) {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_base);
+		TextView tv = (TextView)findViewById(R.id.tV);
+		ConnectivityManager cMan = (ConnectivityManager)getSystemService(CONNECTIVITY_SERVICE);
+		NetworkInfo nInfo = cMan.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+		if (nInfo.isConnected())
+			tv.setText("Connected"); 
+		else
+			tv.setText("Not Connected");
+		String city = "CITY";
+		try {
+			city = getLocation(Locale.getDefault(),getBaseContext(),true,false,false);
+		} catch (IOException e) {
 			// TODO Auto-generated catch block
-			Log.e("LOC_ERR","CANT FIND LOCATION");
+			e.printStackTrace();
 		}
-        
-        if (userLocation != null)
-        	tv.append("\n"+userLocation.toString());
-        else
-        	tv.append("No location available");
-        try {
-			//List<Address> prox = gC.getFromLocation(userLocation.getLatitude(), userLocation.getLongitude(), 1);
-			//List<Address> p2 = gC.getFromLocation(planTwo.getLatitude(), planTwo.getLongitude(), 1);
-			//tv.append("\n"+prox.get(0).getPostalCode().replaceAll(" ", ""));
-			//tv.append("\n"+p2.get(0));
-        	if (lM.isProviderEnabled(lM.GPS_PROVIDER))
-        		tv.append(lM.GPS_PROVIDER);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			tv.append(":::::::");
+		tv.append("\n"+city);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_base, menu);
+		return true;
+	}
+	public String getLocation(Locale locale, Context context, boolean city, boolean postal, boolean state_prov) throws IOException{
+		LocationManager locMan = (LocationManager)this.getSystemService(Context.LOCATION_SERVICE);
+		LocationListener locList = new MyLocList();
+		Geocoder gC = new Geocoder(context,locale);
+		Location gpsLocation = locMan.getLastKnownLocation(locMan.GPS_PROVIDER);
+		locMan.requestLocationUpdates(locMan.NETWORK_PROVIDER, 500, 200, locList);
+		Location networkLocation = locMan.getLastKnownLocation(locMan.NETWORK_PROVIDER);
+		List<Address> addresses = gC.getFromLocation(networkLocation.getLatitude(), networkLocation.getLongitude(), 2);
+
+		
+		if (city){
+			return (gC.getFromLocation(networkLocation.getLatitude(), networkLocation.getLongitude(), 1).get(0).getSubAdminArea());
 		}
-    }
+		else if (postal)
+			return (gC.getFromLocation(networkLocation.getLatitude(), networkLocation.getLongitude(), 1).get(0).getPostalCode());
+		else if (state_prov)
+			return (gC.getFromLocation(networkLocation.getLatitude(), networkLocation.getLongitude(), 1).get(0).getAdminArea());
+		else
+			return "";
+	}
+	public final class MyLocList implements LocationListener
+	{
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_base, menu);
-        return true;
-    }
-    
-    public String getCity(String postalCode)
-    {
-    	final String initURL = "http://maps.googleapis.com/maps/api/geocode/json?address=";
-    	final String finURL = "&sensor=true";
-    	String requestURL = initURL+postalCode+finURL;
-    	return "";
-    }
+		public void onLocationChanged(Location arg0) {
+			// TODO Auto-generated method stub
 
-    
+		}
+
+		public void onProviderDisabled(String provider) {
+			// TODO Auto-generated method stub
+
+		}
+
+		public void onProviderEnabled(String provider) {
+			// TODO Auto-generated method stub
+
+		}
+
+		public void onStatusChanged(String provider, int status, Bundle extras) {
+			// TODO Auto-generated method stub
+
+		}
+	}
+
+	public String getCity(String postalCode)
+	{
+		final String initURL = "http://maps.googleapis.com/maps/api/geocode/json?address=";
+		final String finURL = "&sensor=true";
+		String requestURL = initURL+postalCode+finURL;
+		return "";
+	}
+
+
+
 }
